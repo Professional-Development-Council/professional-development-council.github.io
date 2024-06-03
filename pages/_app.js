7286487ec2507ee511c2e50db73a711131c6ebc5
Parth Govale <133950391+Demolus13@users.noreply.github.com>
2024-06-03 10:46:19 +0530

    authentication

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import '../styles/index.css'
 import '../styles/responsive.css'
 
-// 481922227153-kbtp7vbmkvmvj0jnmvlta46rno1m4lig.apps.googleusercontent.com
-// 191629708651-ct4hu14qejdlrt9nja0ne6l8tj3is8gs.apps.googleusercontent.com
+// CLIENT_ID - 481922227153-kbtp7vbmkvmvj0jnmvlta46rno1m4lig.apps.googleusercontent.com
+// CLIENT_ID - 191629708651-ct4hu14qejdlrt9nja0ne6l8tj3is8gs.apps.googleusercontent.com
 function MyApp({ Component, pageProps }) {
   return (
     <div>

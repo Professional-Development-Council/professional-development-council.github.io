d877579911e4aad9ff69fa0db516e1ce4faa1676
Parth Govale <133950391+Demolus13@users.noreply.github.com>
2024-06-04 13:13:37 +0530

    chore: Remove commented out code for NEXT_PUBLIC_PG_CLIENT_ID in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/index.css'
 import '../styles/responsive.css'
 
 function MyApp({ Component, pageProps }) {
-  const clientId = process.env.NEXT_PUBLIC_PG_CLIENT_ID;
+  // const clientId = process.env.NEXT_PUBLIC_PG_CLIENT_ID;
   return (
     <div>
       <GoogleOAuthProvider clientId="481922227153-kbtp7vbmkvmvj0jnmvlta46rno1m4lig.apps.googleusercontent.com">

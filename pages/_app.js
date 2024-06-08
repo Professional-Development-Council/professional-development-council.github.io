f016d5d007611fb973c9f74d1e147ce80c9f2717
Chandrabhan <chandrabhanpatel321@gmail.com>
2024-06-08 12:37:56 +0530

    updated events page, created events.css,

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import '../styles/index.css'
 import '../styles/responsive.css'
+import '../styles/events.css'; // Import the global CSS file here
+
 
 // PG CLIENT_ID - 481922227153-kbtp7vbmkvmvj0jnmvlta46rno1m4lig.apps.googleusercontent.com
 // AC CLIENT_ID - 191629708651-ct4hu14qejdlrt9nja0ne6l8tj3is8gs.apps.googleusercontent.com
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,22 +1,23 @@
 import React, { useEffect } from "react";
-import { EventsData } from '../../data/EventsData'
+import { EventsData } from '../../data/EventsData';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+// import styles from '../../styles/events.css'; // Import the CSS file
 
 const Events = () => {
   useEffect(() => {
     AOS.init();
-}, [])
+  }, []);
+
   return (
     <div className='main-container'>
       <Head>
-                <title>Events</title>
-                <meta name="description" content="" />
-
-            </Head>
+        <title>Events</title>
+        <meta name="description" content="" />
+      </Head>
       <header id="page-header">
         <div className="page-heading">
           <h2>Events</h2>
@@ -24,34 +25,26 @@ const Events = () => {
       </header>
 
       <div className="page-container">
-        <div className="card-container">
-          <div className="row">
-            {EventsData.map((item,index) => {
-              return (
-                <div className='col-md-4' key={index}>
-                  <div className="card-item" data-aos="fade-up">
-                    <div className="card-item-img">
-                      <Image src={item.image} alt='item' className="Image-general" width={100} height={100}/>
-                    </div>
-                    <div className="card-item-content">
-                      <Link href={`/events/${item.EventName}`} onClick={() => window.scrollTo(0, 0)}><p>{item.EventName}</p></Link>
-
-                    </div>
-
-                  </div>
-
-                </div>
-              )
-            })}
-
-          </div>
-
+        <div className="timeline-container">
+          {EventsData.map((item, index) => (
+            <div className="timeline-item" key={index} data-aos="fade-up">
+              <div className="timeline-content">
+                <h3>{item.EventName}</h3>
+                <p>{item.date}</p>
+                <p>{item.description}</p>
+                <Link href={`/events/${item.EventName}`} passHref>
+                  <button>Read more</button>
+                </Link>
+              </div>
+              <div className="timeline-image">
+                <Image src={item.image} alt='item' className="Image-general" width={100} height={100}/>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;

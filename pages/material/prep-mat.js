403b2045d6878498caf72287e877e290c50f25c5
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2024-06-10 09:28:24 +0000

    updated PrepMat

--- pages/material/prep-mat.js
+++ pages/material/prep-mat.js
@@ -1,89 +1,93 @@
-import React, { useState } from 'react';
-import { PrepMatData } from '../../data/PrepMatData'
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 
 const PrepMat = () => {
-    const [yearFilter, setYearFilter] = useState('All');
-    const uniqueYears = [...new Set(PrepMatData.map(article => article.Year))];
+  const [data, setData] = useState([]);
+  const [yearFilter, setYearFilter] = useState('All');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataPerPage] = useState(20);
+  const scriptURL= "https://script.google.com/macros/s/AKfycbwVZ7kHNm81gFN6M1XBu6oteGUxJZNpYj8_T0Z0R9E7vUKV6NOA7y6T4a_8JYDhcSeq/exec"
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(scriptURL); // Replace with your Google Apps Script URL
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    const filteredArticles = PrepMatData.filter(article => {
-        return (yearFilter === 'All' || article.Year === yearFilter)
-    });
+    fetchData();
+  }, []);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [dataPerPage] = useState(20);
-  
-    const indexOfLastData = currentPage * dataPerPage;
-    const indexOfFirstData = indexOfLastData - dataPerPage;
-    const currentData = filteredArticles.slice(indexOfFirstData, indexOfLastData);
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const uniqueYears = [...new Set(data.map(article => article.Year))];
 
+  const filteredArticles = data.filter(article => {
+    return (yearFilter === 'All' || article.Year === yearFilter);
+  });
 
-    return (
-        <div className='main-container'>
-            <Head>
-                <title>PrepMat</title>
-                <meta name="description" content="" />
+  const indexOfLastData = currentPage * dataPerPage;
+  const indexOfFirstData = indexOfLastData - dataPerPage;
+  const currentData = filteredArticles.slice(indexOfFirstData, indexOfLastData);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-            </Head>
-            <header id="page-header">
-                <div className="page-heading">
-                    <h2>PrepMat</h2>
-                    <h3>Preparation material for Placements and Internships</h3>
-                </div>
-            </header>
-            <div className="page-container">
-                <p>Prepmat is essential for those who wish to access company-specific material for internships and placements. The material serves as an inception to boost your preparation over technical rounds, coding rounds, and aptitude tests. As you lay your hands on Prepmat, you will get acknowledged with the know-how of the different rounds, interview tips, and commonly asked questions.</p>
-
-                <div className="filter-container-item">
-                    Select Year: 
-                    <select className='publication-filter' value={yearFilter} onChange={(e) => setYearFilter(e.target.value)}>
-                        <option value='All'>All</option>
-                        {uniqueYears.map((year, index) => (
-                            <option key={index} value={year}>{year}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <div className='row'>
-                    {currentData.map((item,index) => {
-                        return (
-                            <div className="col-md-3" key={index}>
-                                <div className="card-item">
-                                    <h4>{item.CompanyName}</h4>
-                                    <p>Year: {item.Year}</p>
-                                    {item.MaterialForPlacement && <a href={item.MaterialForPlacement} target='_blank' rel="noreferrer"><button className='card-tags'>Material For Placement</button></a>}
-                                    {item.MaterialForInternship && <a href={item.MaterialForInternship} target='_blank' rel="noreferrer"><button className='card-tags'>Material For Internship</button></a>}
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-
-                <div className="pagination-section">
-
-                        {[...Array(Math.ceil(filteredArticles.length / dataPerPage))].map((_, i) => (
-                            <div key={i} className='pagination-btn-container'>
-                                <button
-                                    className='pagination-btn'
-                                    onClick={() => {
-                                        paginate(i + 1);
-                                        window.scrollTo(0, 0);
-                                    }}
-                                >
-                                    {i + 1}
-                                </button>
-                            </div>
-                        ))}
-
-
-                </div>
-
-            </div>
-
-
+  return (
+    <div className='main-container'>
+      <Head>
+        <title>PrepMat</title>
+        <meta name="description" content="" />
+      </Head>
+      <header id="page-header">
+        <div className="page-heading">
+          <h2>PrepMat</h2>
+          <h3>Preparation material for Placements and Internships</h3>
         </div>
-    )
-}
+      </header>
+      <div className="page-container">
+        <p>Prepmat is essential for those who wish to access company-specific material for internships and placements. The material serves as an inception to boost your preparation over technical rounds, coding rounds, and aptitude tests. As you lay your hands on Prepmat, you will get acknowledged with the know-how of the different rounds, interview tips, and commonly asked questions.</p>
 
-export default PrepMat
+        <div className="filter-container-item">
+          Select Year: 
+          <select className='publication-filter' value={yearFilter} onChange={(e) => setYearFilter(e.target.value)}>
+            <option value='All'>All</option>
+            {uniqueYears.map((year, index) => (
+              <option key={index} value={year}>{year}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className='row'>
+          {currentData.map((item, index) => (
+            <div className="col-md-3" key={index}>
+              <div className="card-item">
+                <h4>{item.CompanyName}</h4>
+                <p>Year: {item.Year}</p>
+                {item.MaterialForPlacement && <a href={item.MaterialForPlacement} target='_blank' rel="noreferrer"><button className='card-tags'>Material For Placement</button></a>}
+                {item.MaterialForInternship && <a href={item.MaterialForInternship} target='_blank' rel="noreferrer"><button className='card-tags'>Material For Internship</button></a>}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="pagination-section">
+          {[...Array(Math.ceil(filteredArticles.length / dataPerPage))].map((_, i) => (
+            <div key={i} className='pagination-btn-container'>
+              <button
+                className='pagination-btn'
+                onClick={() => {
+                  paginate(i + 1);
+                  window.scrollTo(0, 0);
+                }}
+              >
+                {i + 1}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrepMat;
